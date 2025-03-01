75e3a8bef3368f8d7672a9da6cfa874922a3041d
Dahyeon Jin <wlsekgus12@gmail.com>
2025-03-02 00:57:39 +0900

    RspGameVer1 Refactoring

--- chapter_04_20250222/src/RspGameVer1.java
+++ chapter_04_20250222/src/RspGameVer1.java
@@ -6,65 +6,61 @@ public class RspGameVer1 {
     Scanner scanner = new Scanner(System.in);
     Random random = new Random();
 
-    System.out.print("Input player item (rock: 1, scissors: 2, paper: 3) >> ");
+    // 연속적인 숫자 1, 2, 3으로 변경
+    final int ROCK = 1;
+    final int SCISSORS = ROCK + 1;
+    final int PAPER = SCISSORS + 1;
+
+    System.out.print("Input player item (rock: " + ROCK + ", scissors: " + SCISSORS + ", paper: " + PAPER+ ") >> ");
     int playerItem = scanner.nextInt();
-
-    final int MAX_SCORE = 3;
-    final int MIN_SCORE = 1;
-
-    // rock, scissors, paper에 해당하는 숫자가 아닐 시 "오류 문구" 출력 후 프로그램 강제 종료
-    if (playerItem > MAX_SCORE || playerItem < MIN_SCORE) {
-      System.out.println("You input a number that does not correspond to rock, paper, or scissors.");
-      scanner.close();
-      return;
-    }
 
     scanner.close();
 
-    // 컴퓨터는 rock, scissors, paper 아이템을 각각 연속적인 int type 숫자 1, 2, 3으로 랜덤 설정
-    int computerItem = random.nextInt(MIN_SCORE, MAX_SCORE + 1); // 2 + 1하여 최대값을 3으로 맞춤
+    // rock, scissors, paper에 해당하는 숫자가 아닐 시 "오류 문구" 출력 후 프로그램 강제 종료
+    if (playerItem > PAPER || playerItem < ROCK) {
+      System.out.println("You input a number that does not correspond to rock, paper, or scissors.");
+      return;
+    }
 
-    String player = "paper";
-    String computer = "paper";
+    // 컴퓨터는 랜덤한 숫자로 설정
+    int computerItem = random.nextInt(ROCK, PAPER + 1); // 2 + 1하여 최대값을 3으로 맞춤
+
+    System.out.println();
 
     switch (playerItem) {
-      case 3:
-        player = "paper";
+      case ROCK:
+        System.out.println("The player choosed \"rock\".");
         break;
-      case 2:
-        player = "scissors";
+      case SCISSORS:
+        System.out.println("The player choosed \"scissors\".");
         break;
-      case 1:
-        player = "rock";
+      case PAPER:
+        System.out.println("The player choosed \"paper\".");
         break;
     }
 
     switch (computerItem) {
-      case 3:
-        computer = "paper";
+      case ROCK:
+        System.out.println("The player choosed \"rock\".");
         break;
-      case 2:
-        computer = "scissors";
+      case SCISSORS:
+        System.out.println("The player choosed \"scissors\".");
         break;
-      case 1:
-        computer = "rock";
+      case PAPER:
+        System.out.println("The player choosed \"paper\".");
         break;
     }
-
-    System.out.println();
-    System.out.println("The player choosed \"" + player + "\".");
-    System.out.println("The computer choosed \"" + computer + "\".");
 
     System.out.println();
 
     // 바위 > 가위
     // 가위 > 보
     // 보 > 바위
-    if ((player == "rock" && computer == "scissors")
-        || (player == "scissors" && computer == "paper")
-        || player == "paper" && computer == "rock") {
+    if ((playerItem == ROCK && computerItem == SCISSORS)
+        || (playerItem == SCISSORS && computerItem == PAPER)
+        || playerItem == PAPER && computerItem == ROCK) {
       System.out.println("You win!!");
-    } else if (player == computer) {
+    } else if (playerItem == computerItem) {
       System.out.println("Draw.");
     } else {
       System.out.println("You lose...");
