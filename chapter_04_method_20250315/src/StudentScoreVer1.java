8c2257ecaa68b5a5f2f4e44741403ffc8cdd2d48
Dahyeon Jin <wlsekgus12@gmail.com>
2025-03-19 22:43:11 +0900

    Chapter 04. Mission StudentScoreVer1을 구조적 프로그래밍으로 변경

--- chapter_04_method_20250315/src/StudentScoreVer1.java
+++ chapter_04_method_20250315/src/StudentScoreVer1.java
@@ -1,40 +1,25 @@
 import java.util.Scanner;
 
 public class StudentScoreVer1 {
+  public static Scanner scanner = new Scanner(System.in);
+
+  // 각각 점수는 0점 이상 100점 이하이며 잘못 입력 시 오류 문구 출력 후, 강제 종료
+  public static final int MAX_SCORE = 100;
+  public static final int MIN_SCORE = 0;
+
+  // 학생의 국어, 영어, 수학 점수를 입력 받아 합계, 평균, 등급 출력
   public static void main(String[] args) {
-    // 학생의 국어, 영어, 수학 점수를 입력 받아 합계, 평균, 등급 출력
-    Scanner scanner = new Scanner(System.in);
-
-    System.out.print("Input Korean score (0~100) >> ");
+    System.out.print("Input Korean score (" + MIN_SCORE + "~" + MAX_SCORE + ") >> ");
     int korean = scanner.nextInt();
-
-    // 각각 점수는 0점 이상 100점 이하이며 잘못 입력 시 오류 문구 출력 후, 강제 종료
-    final int MAX_SCORE = 100;
-    final int MIN_SCORE = 0;
-
-    if (korean > MAX_SCORE || korean < MIN_SCORE) {
-      System.out.println("You input a number out of range.");
-      scanner.close();
-      return;
-    }
+    printErrorMessage(korean);
 
     System.out.print("Input English score (" + MIN_SCORE + "~" + MAX_SCORE + ") >> ");
     int english = scanner.nextInt();
+    printErrorMessage(english);
 
-    if (english > MAX_SCORE || english < MIN_SCORE) {
-      System.out.println("You input a number out of range.");
-      scanner.close();
-      return;
-    }
-
-    System.out.print("Input Math score (0~100) >> ");
+    System.out.print("Input Math score (" + MIN_SCORE + "~" + MAX_SCORE + ") >> ");
     int math = scanner.nextInt();
-
-    if (math > MAX_SCORE || math < MIN_SCORE) {
-      System.out.println("You input a number out of range.");
-      scanner.close();
-      return;
-    }
+    printErrorMessage(math);
 
     scanner.close();
 
@@ -43,10 +28,41 @@ public class StudentScoreVer1 {
     double average = (double) sum / 3;
     char grade = 'A';
 
-    System.out.println();
-    System.out.println("Your sum score is \'" + sum + "\' score.");
-    System.out.println("Your average score is \'" + average + "\' score.");
+    printSumAndAverage(sum, average);
 
+    grade = calculateGrade(average, grade);
+    printGrade(grade);
+  }
+
+  /**
+   * <p>최소 점수에서 최대 점수 사이의 점수가 아닌 잘못된 점수일 경우 에러 메시지 출력</p>
+   * @param score 과목 점수
+   */
+  public static void printErrorMessage(int score) {
+    if (score > MAX_SCORE || score < MIN_SCORE) {
+      System.out.println("You input a number out of range.");
+      scanner.close();
+      return;
+    }
+  }
+
+  /**
+   * <p>점수의 합계 및 평균 출력</p>
+   * @param sum 점수 합계
+   * @param average 점수 평균
+   */
+  public static void printSumAndAverage(int sum, double average) {
+    System.out.println("\nYour sum score is \'" + sum + "\' score.");
+    System.out.println("Your average score is \'" + average + "\' score.");
+  }
+
+  /**
+   * <p>평균에 따른 등급을 계산하여 반환</p>
+   * @param average 점수 평균
+   * @param grade 등급 초기값: 'A'
+   * @return 평균에 따른 등급
+   */
+  public static char calculateGrade(double average, char grade) {
     int averageOfIntType = (int) average;
 
     switch (averageOfIntType / 10) {
@@ -67,6 +83,14 @@ public class StudentScoreVer1 {
         grade = 'F';
     }
 
+    return grade;
+  }
+
+  /**
+   * <p>등급 출력</p>
+   * @param grade 등급
+   */
+  public static void printGrade(char grade) {
     System.out.println("Your rank is \'" + grade + "\' grade.");
   }
 }
