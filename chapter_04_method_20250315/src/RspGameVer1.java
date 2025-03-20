5858ea189a1d26a400a728f2ad333395c408ed09
Dahyeon Jin <wlsekgus12@gmail.com>
2025-03-20 19:39:42 +0900

    Chapter 04. Mission RspGameVer1을 구조적 프로그래밍으로 변경

--- chapter_04_method_20250315/src/RspGameVer1.java
+++ chapter_04_method_20250315/src/RspGameVer1.java
@@ -2,31 +2,65 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class RspGameVer1 {
+  public static Scanner scanner = new Scanner(System.in);
+
+  // 연속적인 숫자 1, 2, 3으로 변경
+  public static final int ROCK = 1;
+  public static final int SCISSORS = ROCK + 1;
+  public static final int PAPER = SCISSORS + 1;
+
   public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-    Random random = new Random();
-
-    // 연속적인 숫자 1, 2, 3으로 변경
-    final int ROCK = 1;
-    final int SCISSORS = ROCK + 1;
-    final int PAPER = SCISSORS + 1;
-
-    System.out.print("Input player item (rock: " + ROCK + ", scissors: " + SCISSORS + ", paper: " + PAPER+ ") >> ");
-    int playerItem = scanner.nextInt();
-
+    int playerItem = inputPlayerItem();
     scanner.close();
+    printErrorMessage(playerItem);
 
-    // rock, scissors, paper에 해당하는 숫자가 아닐 시 "오류 문구" 출력 후 프로그램 강제 종료
-    if (playerItem > PAPER || playerItem < ROCK) {
-      System.out.println("You input a number that does not correspond to rock, paper, or scissors.");
-      return;
-    }
-
-    // 컴퓨터는 랜덤한 숫자로 설정
-    int computerItem = random.nextInt(ROCK, PAPER + 1); // 2 + 1하여 최대값을 3으로 맞춤
+    // RandomComputerItem: 컴퓨터 아이템을 랜덤한 숫자로 설정
+    int computerItem = RandomNumber();
 
     System.out.println();
+    printItems(playerItem, computerItem);
 
+    System.out.println();
+    printGameResult(playerItem, computerItem);
+  }
+
+  /**
+   * <p>가위바위보에 따른 숫자를 입력 받고, 해당 숫자를 반환</p>
+   * @return 입력한 숫자
+   */
+  public static int inputPlayerItem() {
+    System.out.print("Input player item (rock: " + ROCK + ", scissors: " + SCISSORS + ", paper: " + PAPER + ") >> ");
+
+    return scanner.nextInt();
+  }
+
+  /**
+   * <p>해당 숫자가 rock, scissors, paper에 해당하는 숫자가 아닐 시,</p>
+   * <p>오류 문구 출력 후 프로그램 강제 종료</p>
+   * @param playerItem 검증할 숫자
+   */
+  public static void printErrorMessage(int playerItem) {
+    if (playerItem > PAPER || playerItem < ROCK) {
+      System.out.println("You input a number that does not correspond to rock, paper, or scissors.");
+      System.exit(1); // 프로그램을 강제 종료하려면 return이 아닌, System.exit(1)을 사용해야 함
+    }
+  }
+
+  /**
+   * <p>범위 내의 랜덤한 숫자 생성 후, 반환</p>
+   * @return 랜덤 숫자
+   */
+  public static int RandomNumber() {
+    Random random = new Random();
+    return random.nextInt(ROCK, PAPER + 1); // 2 + 1하여 최대값을 3으로 맞춤
+  }
+
+  /**
+   * <p>플레이어와 컴퓨터가 가위/바위/보 중에 어떤 걸 냈는지 출력</p>
+   * @param playerItem 플레이어가 선택한 아이템
+   * @param computerItem 컴퓨터의 아이템
+   */
+  public static void printItems(int playerItem, int computerItem) {
     switch (playerItem) {
       case ROCK:
         System.out.println("The player choosed \"rock\".");
@@ -50,12 +84,14 @@ public class RspGameVer1 {
         System.out.println("The computer choosed \"paper\".");
         break;
     }
+  }
 
-    System.out.println();
-
-    // 바위 > 가위
-    // 가위 > 보
-    // 보 > 바위
+  /**
+   * <p>가위바위보 게임 결과 출력</p>
+   * @param playerItem 플레이어의 아이템
+   * @param computerItem 컴퓨터의 아이템
+   */
+  public static void printGameResult(int playerItem, int computerItem) {
     if ((playerItem == ROCK && computerItem == SCISSORS)
         || (playerItem == SCISSORS && computerItem == PAPER)
         || playerItem == PAPER && computerItem == ROCK) {
