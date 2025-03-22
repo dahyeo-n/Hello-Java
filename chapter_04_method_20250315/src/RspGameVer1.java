4ff958498cae32849beaae546ac0b2dc9af3ea07
Dahyeon Jin <wlsekgus12@gmail.com>
2025-03-23 00:48:56 +0900

    Chapter 04. Mission RspGameVer1 Feedback 반영

--- chapter_04_method_20250315/src/RspGameVer1.java
+++ chapter_04_method_20250315/src/RspGameVer1.java
@@ -2,41 +2,54 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class RspGameVer1 {
-  public static Scanner scanner = new Scanner(System.in);
-
   // 연속적인 숫자 1, 2, 3으로 변경
   public static final int ROCK = 1;
   public static final int SCISSORS = ROCK + 1;
   public static final int PAPER = SCISSORS + 1;
 
   public static void main(String[] args) {
-    int playerItem = inputPlayerItem();
-    scanner.close();
-    printErrorMessage(playerItem);
+    startRspGame();
+  }
 
-    int computerItem = RandomNumber();
-    printItems(playerItem, computerItem);
+  /**
+   * <p>가위바위보 게임에 필요한 메소드의 집합</p>
+   */
+  public static void startRspGame() {
+    int playerItem = getPlayerItem();
+    int computerItem = randomNumber();
 
+    System.out.println();
+    printItems("player", playerItem);
+    printItems("computer", computerItem);
     printGameResult(playerItem, computerItem);
   }
 
   /**
-   * <p>가위바위보에 따른 숫자를 입력 받고, 해당 숫자를 반환</p>
+   * <p>가위/바위/보에 따른 숫자를 입력 받고, 올바르게 입력했는지 검사 후 입력 받은 숫자 반환</p>
    * @return 입력한 숫자
    */
-  public static int inputPlayerItem() {
-    System.out.print("Input player item (rock: " + ROCK + ", scissors: " + SCISSORS + ", paper: " + PAPER + ") >> ");
+  public static int getPlayerItem() {
+    Scanner scanner = new Scanner(System.in);
+    int playerItem = 0;
 
-    return scanner.nextInt();
+    System.out.print("Input player item (rock: " + ROCK + ", scissors: " + SCISSORS + ", paper: " + PAPER + ") >> ");
+    playerItem = scanner.nextInt();
+
+    validateItem(playerItem, scanner);
+    scanner.close();
+
+    return playerItem;
   }
 
   /**
    * <p>해당 숫자가 rock, scissors, paper에 해당하는 숫자가 아닐 시,</p>
-   * <p>오류 문구 출력 후 프로그램 강제 종료</p>
+   * <p>오류 문구 출력 후 프로그램 강제 종료 ("You input a number that does not correspond to rock, paper, or scissors.")</p>
    * @param playerItem 검증할 숫자
+   * @param scanner Scanner 인스턴스 (`scanner.close()`를 위해 필요)
    */
-  public static void printErrorMessage(int playerItem) {
+  public static void validateItem(int playerItem, Scanner scanner) {
     if (playerItem > PAPER || playerItem < ROCK) {
+      scanner.close();
       System.out.println("You input a number that does not correspond to rock, paper, or scissors.");
       System.exit(1); // 프로그램을 강제 종료하려면 return이 아닌, System.exit(1)을 사용해야 함
     }
@@ -46,40 +59,26 @@ public class RspGameVer1 {
    * <p>범위 내의 랜덤한 숫자 생성 후, 반환</p>
    * @return 랜덤 숫자
    */
-  public static int RandomNumber() {
+  public static int randomNumber() {
     Random random = new Random();
     return random.nextInt(ROCK, PAPER + 1); // 2 + 1하여 최대값을 3으로 맞춤
   }
 
   /**
-   * <p>플레이어와 컴퓨터가 가위/바위/보 중에 어떤 걸 냈는지 출력</p>
+   * <p>플레이어가 가위/바위/보 중에 어떤 걸 냈는지 출력</p>
+   * @param playerName 플레이어의 이름
    * @param playerItem 플레이어가 선택한 아이템
-   * @param computerItem 컴퓨터의 아이템
    */
-  public static void printItems(int playerItem, int computerItem) {
-    System.out.println();
-
+  public static void printItems(String playerName, int playerItem) {
     switch (playerItem) {
       case ROCK:
-        System.out.println("The player choosed \"rock\".");
+        System.out.println("The " + playerName + " choosed \"rock\".");
         break;
       case SCISSORS:
-        System.out.println("The player choosed \"scissors\".");
+        System.out.println("The " + playerName + " choosed \"scissors\".");
         break;
       case PAPER:
-        System.out.println("The player choosed \"paper\".");
-        break;
-    }
-
-    switch (computerItem) {
-      case ROCK:
-        System.out.println("The computer choosed \"rock\".");
-        break;
-      case SCISSORS:
-        System.out.println("The computer choosed \"scissors\".");
-        break;
-      case PAPER:
-        System.out.println("The computer choosed \"paper\".");
+        System.out.println("The " + playerName + " choosed \"paper\".");
         break;
     }
   }
